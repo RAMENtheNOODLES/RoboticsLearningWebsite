1d4d82d15766261979996c4ffdae2f5fc02a45b0
Carter Rommelfanger <github@cookiejar499.me>
2024-05-08 14:37:47 +0000

    stuff

--- robotics-learning-website/src/app/utils/database.ts
+++ robotics-learning-website/src/app/utils/database.ts
@@ -6,4 +6,32 @@ class Database {
     constructor() {
         this.prisma = new PrismaClient();
     }
+
+    /**
+     * Retrieves all the users from the database
+     * @returns a list of users from the data base
+     */
+    getUsers() {
+        this.prisma.$connect()
+
+        let out = null;
+
+        this.prisma.user.findMany().then((users) => {
+            out = users;
+        }).finally(this.prisma.$disconnect);
+
+        return out;
+    }
+
+    getClasses() {
+        this.prisma.$connect()
+
+        let out = null;
+
+        this.prisma.classes.findMany().then((classes) => {
+            out = classes;
+        }).finally(this.prisma.$disconnect);
+
+        return out;
+    }
 }
