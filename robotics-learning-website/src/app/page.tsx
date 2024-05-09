9a46f52526a9986a83f1ec4982bf3dbac3e7ba6b
Carter Rommelfanger <github@cookiejar499.me>
2024-05-09 20:12:01 +0000

    Did stuff

--- robotics-learning-website/src/app/page.tsx
+++ robotics-learning-website/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import { Database } from "./utils/database"
+
+const db = new Database();
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>Hello world</h1>
+        <p>User 1: {db.getUsers()[0]?.username}</p>
       </div>
     </main>
   );
--- robotics-learning-website/src/app/utils/database.ts
+++ robotics-learning-website/src/app/utils/database.ts
@@ -1,6 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 
-class Database {
+export enum Role {
+    STUDENT,
+    TEACHER,
+    ADMIN
+}
+
+export class Database {
     prisma: PrismaClient
 
     constructor() {
@@ -12,25 +18,129 @@ class Database {
      * @returns a list of users from the data base
      */
     getUsers() {
-        this.prisma.$connect()
-
-        let out = null;
-
-        this.prisma.user.findMany().then((users) => {
-            out = users;
-        }).finally(this.prisma.$disconnect);
+        let out: User[] = []
+        this.prisma.$connect().then(() => {
+            this.prisma.user.findMany().then((users) => {
+                users.forEach((user) => {
+                    out.push(user)
+                })
+            }).finally(() => this.prisma.$disconnect());
+        })
 
         return out;
     }
 
     getClasses() {
-        this.prisma.$connect()
+        let out = null;
+        this.prisma.$connect().then(() => {
+            this.prisma.classes.findMany().then((classes) => {
+                out = classes;
+            }).finally(() => this.prisma.$disconnect());
+        })
 
+        return out;
+    }
+
+    getClassTitle(classID: number) {
+        let out = "";
+
+        this.prisma.$connect().then(() => {
+            this.prisma.classes.findFirst({
+                where: {
+                    Id: classID
+                }
+            }).then((c) => {
+                if (c)
+                    out = c.title
+            })
+        }).finally(() => this.prisma.$disconnect());
+
+        return out;
+    }
+
+    getAssignments() {
         let out = null;
 
-        this.prisma.classes.findMany().then((classes) => {
-            out = classes;
-        }).finally(this.prisma.$disconnect);
+        this.prisma.$connect().then(() => {
+            this.prisma.assignment.findMany().then((assignments) => {
+                out = assignments;
+            }).finally(() => this.prisma.$disconnect());
+        });
+
+        return out;
+    }
+
+    getStudentUsername(studentID: number) {
+        let out = "";
+
+        this.prisma.$connect().then(() => {
+            this.prisma.user.findFirst({
+                where: {
+                    Id: studentID,
+                    role: Role.STUDENT
+                }
+            }).then((student) => {
+                if (student)
+                    out = student.username;
+            })
+        }).finally(() => this.prisma.$disconnect());
+
+        return out;
+    }
+
+    /**
+     * Returns a list of Id's of all the assignments a student has
+     * @param studentID the id of the student
+     * @returns a list of the id's of the assignments the student has
+     */
+    getStudentsAssignments(studentID: number) {
+        let out: number[] = [];
+
+        this.prisma.$connect().then(() => {
+            this.prisma.user.findFirst({
+                where: {
+                    Id: studentID
+                },
+                include: {
+                    myAssignments: true
+                }
+            }).then((user) => {
+                if (user) {
+                    user.myAssignments.forEach((assignment) => {
+                        out.push(assignment.Id);
+                    })
+                }  
+            })
+        }).finally(() => this.prisma.$disconnect());
+
+        return out;
+    }
+
+    /**
+     * Returns a list of student id's of every student assigned to an assignment
+     * @param assignmentID the id of the assignment to check
+     * @returns A list of student id's that are assigned
+     */
+    getStudentsAssignedToAssignment(assignmentID: number) {
+        let out: number[] = [];
+        this.prisma.$connect().then(() => {
+            this.prisma.assignment.findFirstOrThrow({
+                where: {
+                    Id: assignmentID
+                },
+                include: {
+                    students: true
+                }
+            }).then((assignment) => {
+                assignment.students.forEach((student) => {
+                    if (student.role == Role.STUDENT)
+                        out.push(student.Id);
+                });
+            }).catch(() => {
+                this.prisma.$disconnect();
+                return out;
+            })
+        }).finally(() => this.prisma.$disconnect());
 
         return out;
     }
